3e0f09449db77ce316d592df8d600276d55fe3bc
joas124 <joasdavi16@gmail.com>
2024-02-15 23:43:44 +0000

    Conversão para YCbCr (SEM ERROS DE PRECISÃO)
    pai é foda

--- PythonMult/projeto1.py
+++ PythonMult/projeto1.py
@@ -49,6 +49,24 @@ def showImg(img, cmap= None, caption= ""):
     plt.axis('off')
     plt.title(caption)
 
+#5.1
+def RGB_to_yCbCr(R, G, B):
+    #converter para YCbCr
+    #depois alterar pro do slide 46 M1.1 (com divisão)
+    Y= 0.299*R + 0.587*G + 0.114*B
+    #Cb= -0.168736*R - 0.331264*G + 0.5*B + 128
+    Cb = ((B - Y) / 1.772) + 128
+    #Cr= 0.5*R - 0.418688*G - 0.081312*B + 128
+    Cr = ((R - Y) / 1.402) + 128
+    return Y, Cb, Cr
+
+def yCbCr_to_RGB(Y, Cb, Cr):
+    #converter para RGB
+    R= np.clip((Cr-128)*1.402 + Y, 0, 255) #clip para valores entre 0 e 255
+    B= np.clip((Cb-128)*1.772 + Y, 0, 255)
+    G= np.clip((Y - (0.299*R) - (0.114*B))/0.587, 0, 255)
+    return R, G, B
+
 
 
 #Main
@@ -67,12 +85,13 @@ def main():
 
     #3.2 colormap
     cm_red= clr.LinearSegmentedColormap.from_list("red", [(0,0,0), (1,0,0)], N= 256) #color, (range de cores rgb do vermelho)
-    cm_green = clr.LinearSegmentedColormap.from_list("red", [(0, 0, 0), (0, 1, 0)], N=256) #256 = 2^8 (8 bits de cores)
-    cm_blue = clr.LinearSegmentedColormap.from_list("red", [(0, 0, 0), (0, 0, 1)], N=256)
-    cm_grey = clr.LinearSegmentedColormap.from_list("red", [(0, 0, 0), (1, 1, 1)], N=256)
+    cm_green = clr.LinearSegmentedColormap.from_list("green", [(0, 0, 0), (0, 1, 0)], N=256) #256 = 2^8 (8 bits de cores)
+    cm_blue = clr.LinearSegmentedColormap.from_list("blue", [(0, 0, 0), (0, 0, 1)], N=256)
+    cm_grey = clr.LinearSegmentedColormap.from_list("grey", [(0, 0, 0), (1, 1, 1)], N=256)
 
     #3.3
     #encoder
+    print("Imagem [0][0]: ", img[0][0])
     R, G, B= encoder(img)
     showImg(R, cmap= cm_red, caption= "Red")
     plt.savefig("imagens/" + fname + "_red.png") #guardar imagens
@@ -88,6 +107,15 @@ def main():
     showImg(imgRec, caption= "Imagem reconstruida: " + fname)
     plt.savefig("imagens/" + fname + "_rec.png")
 
+    Y , Cb, Cr= RGB_to_yCbCr(R, G, B)
+    showImg(Y, cmap= cm_grey, caption= "Y")
+    showImg(Cb, cmap= cm_grey, caption= "Cb")
+    showImg(Cr, cmap= cm_grey, caption= "Cr")
+
+    R, G, B= yCbCr_to_RGB(Y, Cb, Cr)
+    imgRec= decoder(R, G, B, img.shape)
+    showImg(imgRec, caption= "Imagem reconstruida de YCbCr: " + fname)
+    print("Imagem reconstruida YCbCr [0][0]: ", imgRec[0][0])
     plt.show()
 
 if __name__ == "__main__":
